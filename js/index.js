006f4d2901f09a5b4e9ce90f602f96b461608f93
elillo <elilloperez35@gmail.com>
2018-09-07 08:16:52 +0100

    Primer proyecto the ironbeer slot game (en proceso)

--- js/index.js
+++ js/index.js
@@ -1,68 +1,92 @@
-var Slot = function (id) {
+var Slot = function (id, fase) {
     this.contador = 0;
     this.tiempo;
     this.images = [];
-    this.id=id;
-    for (var i = 0; i < 9; i++) {
-        this.images.push("images/imagenesSlotPrincipal/cerveza(" + i + ").jpeg")
+    this.id = id;
+    this.fase = fase;
+    this.numImage = (fase == "One") ? 3 : 9;
+
+    //introduzco las imagenes en las arrays del constructor con su fase asociada y su id(0,1,2...)
+
+    for (var i = 0; i < this.numImage; i++) {
+        this.images.push("../images/Slot/" + fase + i + ".jpeg")
     }
-   
-    $("#contenido").append("<div class='slot' id='slot_" + id + "'></div>");
-    this.create();   
+
+    //Introduzco en las secciones del html los divs coincidentes con la fase en la que estemos y llamo a la funcion create.
+    $("#" + fase).append("<div class='slot' id='" + fase + id + "'></div>");
+    this.create(this.images);
+
 }
+
 
 //SEGUNDA llamada del flujo. Creamos el prototype move y le damos como valor una función.
 Slot.prototype.move = function () {
-//en esta funcion movemos la ultima imagen del slot y la la colocamos al comienzo de las imágenes. 
-
-//evitamos problemas con el scope del constructor alojando this en la variable self.       
+    //en esta funcion movemos la ultima imagen del slot y la colocamos al comienzo de las imágenes. 
+    //evitamos problemas con el scope del constructor alojando this en la variable self.       
     var self = this;
-//apuntamos al  ultimo div de un slot + su Id y la alojamos en la variable ultimaImg.
-    var ultimaImg = $("#slot_" + self.id + " div:last")
-     var slot= $("#slot_" + self.id)                                                 //DUDA
+    //apuntamos al  ultimo div de un slot con su Id y la alojamos en la variable ultimaImg y lo mismo para ultimaImg2.
+    var ultimaImg = $("#" + self.fase + self.id + " div:last");
+    var slot = $("#" + self.fase + self.id);
 
-//aplicamos el metodo detach a  ultimaImg,que elimina el elemento,conservando sus datos y eventos asociados y guarda una copia para reutilizarlos.
-//en este caso insertarlo en la posición anterior al primero del slot.
+    //aplicamos el metodo detach a  ultimaImg y a ultimaImg2.Este método elimina el elemento,conservando sus datos y eventos asociados y guarda una copia para reutilizarlos.
+    //en este caso insertarlo en la posición anterior al primero del slot.
     ultimaImg.detach().prependTo(slot);
- }
+}
 
- 
- // TERCERA llamada del flujo.Función create,encargada de introducir en el dom los divs de las imagenes.
-Slot.prototype.create = function (cb) {
+
+// TERCERA llamada del flujo.Función create,encargada de introducir en el dom los divs de las imagenes.
+Slot.prototype.create = function () {
     var self = this
-    for (var i = 0; i < 9; i++) {
-        $("#slot_" + self.id).append(
-            "<div id='image_" + i + "'><img src='" + self.images[i] + "'</div>"
-        );
-    }
-    if (cb)                                                                                 //DUDA
-        cb();
+
+    for (var i = 0; i < self.numImage; i++) {
+        //Este bucle introducen las imagenes en sus slots correspondiestes.
+        $("#" + self.fase + self.id).append("<div id='image_" + i + "'><img src='" + this.images[i] + "'</div>");
+
     }
 
+}
 
 //PRIMERA llamada del flujo. Creamos el prototype start y le damos como valor una función.
 Slot.prototype.start = function () {
     var self = this
     self.tiempo = 0;
-//guardo en numVueltas el random de entre 9 posiciones,estas se refiren al numero de imagenes de cada slot. 
-    var numVueltas = Math.floor(Math.random() * (34 - 25)) + 25;
-    
+
+    //guardo en numVueltas el random de entre 9 o 3 posiciones depende de la fase,estas se refiren al numero de imagenes de cada slot. 
+
+   
+      //var numVueltas = Math.floor(Math.random() * (34 - 25)) + 15;                                                                             //RANDOM
+         var numVueltas = 16;
     console.log('numVueltas', numVueltas)
-//incio un bucle for partiendo desde contador que vale 0, y que itere mientras numVueltas sea mayor que contador,
-//este numero de vueltas me lo da el random.
-    
+
+    //incio un bucle for partiendo desde contador que vale 0, y que itere mientras numVueltas sea mayor que contador(numero de iteraciones a realizar),
+    //este numero de vueltas me lo da el random.
     for (self.contador = 0; self.contador < numVueltas; self.contador++) {
-//en esta variable rasigno el valor de tiempo que es contador(numero de iteraciones) * 10.
+        //en esta variable rasigno el valor de tiempo que es contador(numero de iteraciones) * 10.
         self.tiempo = self.tiempo + self.contador * 10;
 
         console.log(self.tiempo, self.contador);
-//aplico un setTimeout pasandole como parametro vuelta para ver en consola el numero de vuelta en el que está el slot
-// y la variable numVueltas,donde se aloja el numero de vueltas resultante para tener acceso a dicho valor.
+        //aplico un setTimeout pasandole como parametro vuelta para ver en consola el numero de vuelta en el que está el slot
+        // y la variable numVueltas,donde se aloja el numero de vueltas resultante para tener acceso a dicho valor.
         setTimeout(function (vuelta, numVueltas) {
-//pasado el tiempo alojado en la propiedad tiempo llamo a la función move().
+            //pasado el tiempo alojado en la propiedad tiempo llamo a la función move().
             self.move();
-            console.log(vuelta)
-//porque le pasamos aqui tres argumentos?
+
+            // Si ya ha terminado de dar las vueltas,es lo que dice esta condición,apunto a cada uno de los slots y saco el atributo que es la fas
+            //y su id.
+            if (vuelta == numVueltas - 1 && self.id == 3) {
+                var slot1 = $("#" + self.fase + " #" + self.fase + "1 div:first").attr("id"); //#Two #Two1 div:first
+                var slot2 = $("#" + self.fase + " #" + self.fase + "2 div:first").attr("id");
+                var slot3 = $("#" + self.fase + " #" + self.fase + "3 div:first").attr("id");
+
+                //Con esto llamamos a nuestro propio evento que es cambio fase
+                if (slot1 == slot2 && slot2 == slot3) {
+
+                    $(document).trigger("cambioFase");
+
+                }
+
+            };
+
         }, self.tiempo, self.contador, numVueltas);
 
     }
--- js/main.js
+++ js/main.js
@@ -1,46 +1,65 @@
-$(document).ready(function(){
-  var slot1=new Slot(1)
-  var slot2=new Slot(2)
-  var slot3=new Slot(3)
-  // var slot4=new Slot(4)
-  // var slot5=new Slot(5)
-  // var slot6=new Slot(6)
-  $("#sectOneId").removeClass("sectOne").addClass("sectOneDefault");
-  $(".timeBeer").hide();
-//Al pulsar el boton llamamos a la función start e iniciamos el flujo.
-  $("#play").click(function(){
-    slot1.start();
-  
-   setTimeout (function(){
-    
-    slot2.start()
-    
-    }, 500);
-    
-   setTimeout (function(){
-  
-    slot3.start()
-        
-    }, 1000);
-        
-  });
+$(document).ready(function () {
+  //Creo las instancias de cada slot con los argumento que se alojarán en el constructor,en this.id y this.fase
+  var slot1 = new Slot(1, "One")
+  var slot2 = new Slot(2, "One")
+  var slot3 = new Slot(3, "One")
+  var slot4 = new Slot(1, "Two")
+  var slot5 = new Slot(2, "Two")
+  var slot6 = new Slot(3, "Two")
+  //Con esta varible indico la fase por defecto(slots);
+  var fase = "Two"
 
-  // $("#play").click(function(){
-  //   slot4.start();
-    
-  //    setTimeout (function(){
-      
-  //   slot5.start()
-      
-  //     }, 500);
-      
-  //   setTimeout (function(){
-    
-  //   slot6.start()
-          
-  //     }, 1000);
-          
-          
-    // });
-  
+  //oculto los elementos de pantalla de bonus.
+  $(".timeBeer").hide();
+
+
+  //Al pulsar el botón llamamos a la función start e iniciamos el flujo.
+  $("#play").click(function () {
+    //si estamos en la fase One se ejecutan los slots del 1 al 3
+    if (fase == "One") {
+
+      slot1.start();
+      //Las llamo con diferentes intervalos de tiempo para que no rueden a la vez.
+      setTimeout(function () {
+        slot2.start()
+      }, 400);
+
+      setTimeout(function () {
+        slot3.start()
+      }, 800);
+      ////si estamos en la fase Two se ejecutan los slots del 4 al 6
+
+    } else {
+
+      slot4.start();
+      //Las llamo con diferentes intervalos de tiempo para que no rueden a la vez.
+      setTimeout(function () {
+        slot5.start()
+      }, 500);
+
+      setTimeout(function () {
+        slot6.start()
+      }, 1000);
+    }
+
+  });
+  //Aqui organizo todos los eventos que se tienen q dar cuando cambia la fase,para eso creo este evento personalizado.  
+  $(document).on("cambioFase", function () {
+    //  console.log("cambioFase",fase);
+    fase = (fase == "One") ? "Two" : "One";
+
+    if (fase == "One") {
+
+      $("#CubrirSlotArriba").slideUp(2000);
+      $(".timeBeer").fadeIn(2500);
+
+    }
+
+  });
+  // if(".Two1" == ".Two3"){ 
+
+  //   $(".twoBeerUp").css("height","300px");
+
+  // }
+
 });
